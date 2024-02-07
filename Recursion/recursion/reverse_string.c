0bba02037d39cfc350269b55fcbe33d64ba9607d
arjunmishraaa <154399579+arjunmishraaa@users.noreply.github.com>
2024-02-07 23:15:12 +0530

    second commit

--- Recursion/recursion/reverse_string.c
+++ Recursion/recursion/reverse_string.c
@@ -1,24 +1,17 @@
 #include<stdio.h>
-char reverse_string(int a, int b,char* str){
-    if(a<b){
-       char temp= str[a];
-        str[a]=str[b];
-        str[b]=temp;
-        reverse_string(a+1,b-1,str);
-    }
-    // else printf("Invalid input!!\nThe Range should be from low to high!!");
-}
+#include<string.h>
+void rev(char str[], int n){
+     if(n<0) return;
+     printf("%c",str[n]);
+     return rev(str,n-1);
 
-
-int main(){
-    int n; 
-    printf("Enter the size: ");
-    scanf("%d",&n);
-    char str[n];
-    printf("Eneter string: ");
     
-    int a,b;
-    printf("Enter the limit to reverse:\n");
-    scanf("%d %d",&a,&b);
-    reverse_string(a,b,str[n]);
+}
+int main()
+{
+    int n;
+    char str[200];
+    scanf("%s",&str);
+    n=strlen(str);
+    rev(str,n);
 }
